9aa9a54fe38251821929766104407350ff5f7190
root <root@orchestrator.c.mondadorici.internal>
2018-01-12 21:32:58 +0000

    clean up

--- terraform/devops/staging-ci-server/staging-ci-server.tf
+++ terraform/devops/staging-ci-server/staging-ci-server.tf
@@ -9,7 +9,7 @@ resource "google_compute_instance" "ci-server" {
  project = "mondadorici"
  zone = "us-east1-b"
  name = "staging-ci"
- machine_type = "n1-standard-4"
+ machine_type = "n1-standard-2"
 
  tags = ["jenkins", "cloudera", "http", "https"]
  
@@ -26,10 +26,10 @@ resource "google_compute_instance" "ci-server" {
    }
  }
 
- provisioner "local-exec" {
+# provisioner "local-exec" {
  
-  command = "sleep 90; ansible-playbook -i '${google_compute_instance.ci-server.name},' --private-key=~/.ssh/ansible_rsa /opt/ansible/staging-ci.yml -e 'ansible_ssh_user=dario_pasquali93' -e 'host_key_checking=False'"
- }
+#  command = "sleep 90; ansible-playbook -i '${google_compute_instance.ci-server.name},' --private-key=~/.ssh/ansible_rsa /opt/ansible/staging-ci.yml -e 'ansible_ssh_user=dario_pasquali93' -e 'host_key_checking=False'"
+# }
 }
 
 output "ci-server" {
